414b079d01241765ec2f1456254b467f2a55d4db
codykraz <bassguitarninja@gmail.com>
2014-04-13 19:46:41 -0400

    Added select

--- student_P4/select.cpp
+++ student_P4/select.cpp
@@ -16,7 +16,44 @@ Status Operators::Select(const string & result,      // name of the output relat
 		         const Operator op,         // predicate operation
 		         const void *attrValue)     // literal value in the predicate
 {
-
+	int numAtts;
+	AttrDesc* attArray; //delete when done
+	attrCat->getRelInfo(attr->relName, numAtts, attArray);
+	
+	int len = 0;
+	const AttrDesc pNames[projCnt];
+	for (int i =0; i< projCnt; i++){
+		AttrDesc* temp = new AttrDesc();
+		attrCat->getInfo(attr->relName, projNames[i].attrName, *temp);
+		pNames[i] = *temp;
+		len+=temp->attrLen;
+	}
+	const int reclen = len;
+	
+	AttrDesc* attrDesc = new AttrDesc();
+	attrCat->getInfo(attr->relName, attr->attrName, *attrDesc);
+	
+	
+	
+	
+	
+	//if equality predicate and has an index
+	//
+	if (op == EQ && attrDesc->indexed) {
+		Operators::IndexSelect(result,projCnt,
+                              pNames, attrDesc,   
+                              op, attrValue, reclen);
+                              
+          //error check
+     }
+                              
+     else {
+     Operators::ScanSelect(result, projCnt, 
+                             pNames, attrDesc, 
+                            op, attrValue, reclen);
+                            
+     	//error check
+     }
+	
 return OK;
 }
-
